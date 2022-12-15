7b856139518137164ef4a0bfaf2d1fd2d5d2022b
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-12-15 16:22:05 +0000

    Fixed cors error by installing and using cors (dev-dependency)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { teamsRouter } from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/teams", teamsRouter);
 
