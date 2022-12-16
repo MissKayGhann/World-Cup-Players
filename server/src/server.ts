fd4622bf219079910e0963c3ef843a0fb93191f4
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-12-16 14:11:50 +0000

    Make cors dependency instead of dev dependency

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,7 @@ import cors from "cors";
 import { teamsRouter } from "./routes";
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json(), cors());
 
 app.use("/teams", teamsRouter);
 
