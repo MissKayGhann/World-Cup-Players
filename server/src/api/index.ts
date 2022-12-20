58027406f72bc52e115967d84c47df4a5a39e4c9
Scott Robinson <scott.robinson2@sky.uk>
2022-12-20 10:35:30 +0000

    EOL for branch -- API has missing players so migrating to a different one

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -8,12 +8,24 @@ import {
     PlayerKeysFromDynamoDB,
     PlayerName,
 } from "./types";
-import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import * as stream from "node:stream";
 import { ddbdClient } from "../lib";
+import { ScanCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { S3Client } from "@aws-sdk/client-s3";
+import { ReadableStream } from "node:stream/web";
+import { Upload } from "@aws-sdk/lib-storage";
 
 if (process.env.FUT_API_KEY === undefined) {
     throw new Error("Failed to load FUT_API_KEY from .env file");
 }
+
+const client = new S3Client({
+    region: "eu-west-2",
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.SECRET_ACCESS_KEY || "",
+    },
+});
 
 /**
  * Fetches the nations and names of all football players in DynamoDB.
@@ -111,9 +123,39 @@ export async function fetchPlayerToIdMapping(
     // - we run out of pages (raises an error)
     // - we have all 831 player IDs that we need (will then return them)
     while (playersFound !== 831) {
+        if (page % 20 === 0) console.log(`Found ${playersFound} players before page ${page}.`);
         if (page > maxPage) {
             // This *should* never happen, but we handle it just in case
-            throw new Error(`Exceeded page limit of ${maxPage} when trying to find players`);
+            const missingPlayers: Record<Nation, Player["name"][]> = {};
+            for (let nation of Object.keys(nationToPlayerNames)) {
+                if (nationToPlayers[nation] === undefined) {
+                    missingPlayers[nation] = nationToPlayerNames[nation];
+                    continue;
+                }
+
+                for (let playerName of nationToPlayerNames[nation]) {
+                    let playerHasBeenRetrieved = false;
+                    for (let retrievedPlayer of nationToPlayers[nation]) {
+                        if (retrievedPlayer.name === playerName) {
+                            playerHasBeenRetrieved = true;
+                            break;
+                        }
+                    }
+                    if (!playerHasBeenRetrieved) {
+                        if (missingPlayers[nation] === undefined)
+                            missingPlayers[nation] = [playerName];
+                        else missingPlayers[nation].push(playerName);
+                    }
+                }
+            }
+            throw new Error(
+                `Exceeded page limit of ${maxPage} when trying to find players. ` +
+                    `Found ${playersFound} / 831 players and are missing the following players:\n${JSON.stringify(
+                        missingPlayers,
+                        undefined,
+                        4
+                    )}`
+            );
         }
 
         const resp = await fetch(`https://futdb.app/api/players?page=${page}`, {
@@ -136,14 +178,40 @@ export async function fetchPlayerToIdMapping(
                     ]) {
                         if (storedPlayerName === player.name) {
                             // We need this player's id
+                            let playerSeenBefore = false;
+
                             if (nationToPlayers[nation] === undefined) {
                                 // Nation hasn't been seen
                                 nationToPlayers[nation] = [{ name: player.name, id: player.id }];
                             } else {
-                                // The nation has been seen
-                                nationToPlayers[nation].push({ name: player.name, id: player.id });
+                                // The nation has been seen, so check if the player has been seen
+                                for (let i = 0; i < nationToPlayers[nation].length; i++) {
+                                    const seenPlayer = nationToPlayers[nation][i];
+                                    if (seenPlayer.name === player.name) {
+                                        // Player has been seen before, so set the id to the lower of the two.
+                                        // This needs to be done because the API may have the same player multiple
+                                        // times, e.g. a rare and hero version of the player. We want the player with
+                                        // the lowest id, as that's the "base" player (the non-special player).
+                                        console.log(
+                                            `Refound ${nation}'s ${player.name}. ` +
+                                                "Setting the id to the lower of: " +
+                                                `${player.id} (new) and (${seenPlayer.id}) (old)`
+                                        );
+                                        nationToPlayers[nation][i].id = Math.min(
+                                            seenPlayer.id,
+                                            player.id
+                                        );
+                                        playerSeenBefore = true;
+                                        break;
+                                    }
+                                }
+                                if (!playerSeenBefore)
+                                    nationToPlayers[nation].push({
+                                        name: player.name,
+                                        id: player.id,
+                                    });
                             }
-                            playersFound++;
+                            if (!playerSeenBefore) playersFound++;
                         }
                     }
                 }
@@ -155,45 +223,86 @@ export async function fetchPlayerToIdMapping(
 }
 
 /**
- * Gets the photo of the specified player from the FUT API
- * @param playerId The ID of the player to get the photo of
+ * Gets the photo of the specified player from the FUT API.
+ * @param playerId The ID of the player to get the photo of.
  */
 async function getPlayerPhoto(playerId: number) {
-    console.log("getPlayerPhoto", playerId);
+    const resp = await fetch(`https://futdb.app/api/players/${playerId}/image`);
+    return resp.body;
 }
 
 /**
  * Stores the player's photo in S3 and returns its object URl.
- * @param nation The nation of the player
- * @param player The player's name
- * @param photo The player's photo
- * @returns The stored photo's object URL.
+ * @param nation The nation of the player.
+ * @param player The player object for the player whos photo is being stored.
+ * @param photoStream A stream containing the player's photo.
+ * @returns Info on the uploaded photo.
  */
-async function storePhotoInS3(nation: string, player: string, photo: any) {
-    console.log("storePhotoInS3", nation, player, photo);
-    return "the-stored-photo-object-url";
+async function storePhotoInS3(nation: string, player: Player, photoStream: stream.Readable) {
+    const upload = new Upload({
+        client: client,
+        params: {
+            Bucket: "worldcupimages",
+            Key: `players/${nation.replace(" ", "_")}/${player.name.replace(" ", "_")}.png`,
+            Body: photoStream,
+        },
+    });
+
+    upload.on("httpUploadProgress", progress => {
+        console.log(
+            `Loaded part ${progress.part} of ${nation}'s ${player.name} (id: ${player.id})`
+        );
+    });
+
+    return await upload.done();
 }
 
 /**
- * Stores the provided S3 Object URL of the player's photo in DynamoDB
- * @param nation The nation of the player
- * @param player The player's name
- * @param photo The player's photo
+ * Stores the provided S3 Object URL of the player's photo in DynamoDB.
+ * @param nation The nation of the player.
+ * @param player The player object for the player whos photo is being stored.
+ * @param photoUrl A url pointing to the player's photo.
  */
-async function storePhotoInDynamoDB(nation: string, player: string, photoURL: string) {
+async function storePhotoInDynamoDB(nation: string, player: Player, photoURL: string) {
     console.log("storePhotoInDynamoDB", nation, player, photoURL);
+    return await ddbdClient.send(
+        new UpdateCommand({
+            TableName: "Players",
+            Key: {
+                team: nation,
+                name: player.name,
+            },
+            UpdateExpression: "SET photo = :photoUrl",
+            ExpressionAttributeValues: {
+                ":photoUrl": photoURL,
+            },
+        })
+    );
 }
 
 /**
- * A helper function responsible for calling `storePhotoInS3` and `storePhotoDynamoDB`
+ * A helper function responsible for calling `storePhotoInS3` and `storePhotoInDynamoDB`.
  * @param nationId The nation ID of the player's team.
  * @param playerId The ID of the player.
  * @param photo The photo of the player to store in S3 & link to in DynamoDB.
  */
-async function storePlayerPhoto(nation: string, player: string, photo: any) {
-    console.log("storePlayerPhoto", typeof photo);
-    const objectURL = await storePhotoInS3(nation, player, photo);
-    await storePhotoInDynamoDB(nation, player, objectURL);
+async function storePlayerPhoto(nation: string, player: Player, photoStream: stream.Readable) {
+    const s3Response = await storePhotoInS3(nation, player, photoStream);
+
+    // if (!isCompleteMultipartUploadCommandOutput(s3Response)) {
+    //     throw new Error(
+    //         `Failed to upload photo of ${nation}'s ${player.name} (id: ${player.id}) to s3.`
+    //     );
+    // }
+
+    if (!("Location" in s3Response) || s3Response.Location === undefined) {
+        // Upload to s3 was unsuccessful or the `Location` field is `undefined`.
+        throw new Error(
+            `Location of the s3 object for ${nation}'s ${player.name} (id: ${player.id}) was undefined.`
+        );
+    }
+    // Upload to s3 was successful, *and* we have the url of the uploaded photo.
+    return storePhotoInDynamoDB(nation, player, s3Response.Location);
 }
 
 /**
@@ -203,9 +312,33 @@ async function storePlayerPhoto(nation: string, player: string, photo: any) {
  */
 export async function fetchAndStorePlayerPhotos(nationToPlayers: Record<Nation, Player[]>) {
     for (let [nation, players] of Object.entries(nationToPlayers)) {
-        for (let player of players) {
-            const photo = getPlayerPhoto(player.id);
-            storePlayerPhoto(nation, player.name, photo);
+        const promises = players.map(async player => {
+            const photoStream = stream.Readable.from(
+                (await getPlayerPhoto(player.id)) as ReadableStream<any>
+            );
+
+            return storePlayerPhoto(nation, player, photoStream);
+        });
+
+        let failed = 0;
+        const results = await Promise.allSettled(promises);
+        for (let result of results) {
+            if (result.status === "rejected") {
+                // Photo failed to upload to s3 or dynamodb
+                console.log(result.reason);
+                failed++;
+            } else {
+                // Photo suceeded in being uploaded
+                console.log(result.value);
+            }
         }
+        console.log(
+            `${players.length - failed}/${players.length} suceeded (${(
+                (1 - failed / players.length) *
+                100
+            ).toFixed(2)}%)`
+        );
+
+        break; // only do the first team's players
     }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { teamsRouter } from "./routes";
 import {
+    fetchAndStorePlayerPhotos,
     fetchNationToIdMapping,
     fetchPlayerNamesAndTeams,
-    fetchAndStorePlayerPhotos,
     fetchPlayerToIdMapping,
 } from "./api";
 import { Nation, Player, PlayerName } from "./api/types";
+import * as playerJSON from "./output.json";
+console.log(playerJSON);
+
+import fs from "node:fs";
+import path from "node:path";
+
 const app = express();
 app.use(express.json());
 
@@ -17,13 +23,53 @@ app.listen(5001, async () => {
         Set<Nation>,
         Record<Nation, PlayerName[]>
     ];
-
+    console.log("Retrieved players and teams from dynamodb...");
     const nationToId = await fetchNationToIdMapping(nations);
+    console.log("Retrieved mapping of a nation to its id...");
     const nationToPlayers: Record<Nation, Player[]> = await fetchPlayerToIdMapping(
         nationToId,
         nationToPlayerNames
     );
+    console.log("Retrieved mapping of a player to its id...");
+    // const nationToPlayers: Record<Nation, Player[]> = playerJSON;
+
+    // const newPlayersJSON: Record<Nation, Player[]> = {};
+    // for (let [nation] of Object.keys(nationToPlayers)) {
+    //     newPlayersJSON[nation] = [];
+
+    //     console.log(nationToPlayers[nation]);
+    //     for (let player of nationToPlayers[nation]) {
+    //         let alreadyStored = false;
+    //         for (let i = 0; i < newPlayersJSON[nation].length; i++) {
+    //             const storedPlayer = newPlayersJSON[nation][i];
+    //             if (player.name === storedPlayer.name) {
+    //                 newPlayersJSON[nation][i].id = Math.min(player.id, storedPlayer.id);
+    //                 alreadyStored = true;
+    //                 break;
+    //             }
+    //         }
+    //         if (!alreadyStored) {
+    //             newPlayersJSON[nation].push(player);
+    //         }
+    //     }
+    // }
+
+    fs.writeFile(
+        path.join(__dirname, "..src/newOutput.json"),
+        JSON.stringify(nationToPlayers, undefined, 4),
+        err => {
+            if (err) {
+                console.error(err);
+            }
+            console.log("Successfully wrote to output.json");
+        }
+    );
+
+    return;
+
+    console.log("Retrieved mapping of a player to its id...");
     await fetchAndStorePlayerPhotos(nationToPlayers);
+    console.log("Stored player photos");
 
     console.log("Listening on port 5001");
 });
